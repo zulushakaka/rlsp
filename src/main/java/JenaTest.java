5c033acc1387d0fc50155158428b3a92bda47eff
Xianyang <chenxianyang.tom@gmail.com>
2018-04-21 19:57:39 -0400

    init commit

---
+++ src/main/java/JenaTest.java
@@ -0,0 +1,22 @@
+import org.apache.jena.query.ReadWrite;
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.tdb2.TDB2Factory;
+import org.apache.jena.query.Dataset;
+
+
+public class JenaTest {
+
+    public static void main(String[] args) {
+        String directory = "/home/xianyang/kb/d-fb/";
+        Dataset dataset = TDB2Factory.connectDataset(directory);
+
+        dataset.begin(ReadWrite.READ);
+        Model model = dataset.getDefaultModel();
+
+        String jb = "<http://rdf.freebase.com/ns/m.06w2sn5>";
+        model.getResource(jb);
+
+        dataset.end();
+    }
+
+}
